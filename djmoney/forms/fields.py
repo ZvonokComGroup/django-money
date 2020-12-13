a3c0eeaf370d512ca394b33897732201b9ae57fb
horpto <__singleton__@hackerdom.ru>
2020-12-13 20:06:19 +0300

    fix form

--- djmoney/forms/fields.py
+++ djmoney/forms/fields.py
@@ -35,7 +35,7 @@ class MoneyField(MultiValueField):
             **kwargs
         )
         currency_field = ChoiceField(choices=currency_choices)
-        currency_field.widget.attrs['disabled'] = len(currency_choices) == 1
+        currency_field.widget.attrs['readonly'] = len(currency_choices) == 1
 
         self.widget = MoneyWidget(
             amount_widget=amount_field.widget,
